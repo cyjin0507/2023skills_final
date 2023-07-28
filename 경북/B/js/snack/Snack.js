706e65a0010cfdcce3dd4d015206ca709cf9ea44
choiyoungjin <cyjin0507@gmail.com>
2023-07-28 16:40:09 +0900

    commit

--- 경북/B/js/snack/Snack.js
+++ 경북/B/js/snack/Snack.js
@@ -12,6 +12,7 @@ class Game {
 
         this.length = 5
         this.posArr = []
+        this.lastPos = 0
 
         // 0 : 아래 / 1 : 위 / 2 : 왼 / 3 : 오
         this.forward = 0
@@ -47,7 +48,7 @@ class Game {
         $(document).on('keydown', this.changePos.bind(this))
 
         this.charactorMove = setInterval(this.autoMove.bind(this), 500)
-        this.gameProcess = setInterval(this.game.bind(this), 1000 / 15)
+        // this.gameProcess = setInterval(this.game.bind(this), 1000 / 15)
     }
 
     itemLoc() {
@@ -73,11 +74,16 @@ class Game {
         } else if (this.forward == 3) {
             this.posX++
         }
+    
+        this.lastPos = this.posArr[this.posArr.length-1]
 
         this.posArr.pop()
         this.posArr.unshift(
             { x: this.pos.x, y: this.pos.y }
         )
+
+        this.game()
+
     }
 
     setting() {
@@ -99,26 +105,17 @@ class Game {
             this.upDown = 1
             this.forward = 1
             this.create = 0
-            this.posY--
         } else if (e.keyCode == 39) {
             this.forward = 3
             this.create = 2
-            this.posX++
         } else if (e.keyCode == 40) {
             this.forward = 0
             this.upDown = 0
             this.create = 1
-            this.posY++
         } else if (e.keyCode == 37) {
             this.forward = 2
             this.create = 3
-            this.posX--
         }
-
-        this.posArr.pop()
-        this.posArr.unshift(
-            { x: this.pos.x, y: this.pos.y }
-        )
 
         clearInterval(this.charactorMove)
 
@@ -139,11 +136,11 @@ class Game {
 
         this.blockLine()
         this.randomItem()
-        
-        for (let i = 0; i < this.posArr.length; i++) {
-            this.ctx.fillStyle = i==0 ? "green" : "red"
-            this.ctx.fillRect(this.posArr[i].x, this.posArr[i].y, this.blockWidth, this.blockHeight)
-        }
+
+        this.frame = 0
+        requestAnimationFrame(this.requestAnimation)
+
+
 
         // 아이템 먹었는지 확인
         if(this.posX == this.itemPosX && this.posY == this.itemPosY) {
@@ -162,6 +159,43 @@ class Game {
             this.crushCheck = true
         } else {
             this.crushCheck = false
+        }
+    }
+    
+    requestAnimation = () => {
+        if(this.frame >= 1) {
+            return
+        }
+        this.frame = Math.min(1, this.frame + 0.01)
+        requestAnimationFrame(this.requestAnimation)
+                // 실제 뱀 움직임
+        this.animation()
+    }
+
+    // this.ctx.fillRect(this.posArr[i].x, before.y + (this.blockHeight * this.frame), this.blockWidth, this.blockHeight)
+
+    animation() {
+        for (let i = 0; i < this.posArr.length; i++) {
+            this.ctx.fillStyle = i==0 ? "green" : "red"
+            let before = this.posArr[i+1] ? this.posArr[i+1] : this.lastPos
+
+            if(this.posArr[i].x - before.x == 0) {
+                // if(i==4) {
+                //     this.ctx.fillStyle = 'white'
+                //     this.ctx.fillRect(this.posArr[i].x, before.y, this.blockWidth, this.blockHeight * this.frame)
+                // }
+                if(this.forward==0) {
+                    this.ctx.fillRect(this.posArr[i].x, before.y + (this.blockHeight * this.frame), this.blockWidth, this.blockHeight)
+                } else if(this.forward == 1) {
+                    this.ctx.fillRect(this.posArr[i].x, before.y - (this.blockHeight * this.frame), this.blockWidth, this.blockHeight)
+                }
+            } else if(this.posArr[i].y - before.y == 0) {
+                if(this.forward==2) {
+                    this.ctx.fillRect(before.x - (this.blockWidth * this.frame), this.posArr[i].y, this.blockWidth, this.blockHeight)
+                } else if(this.forward==3) {
+                    this.ctx.fillRect(before.x + (this.blockWidth * this.frame), this.posArr[i].y, this.blockWidth, this.blockHeight)
+                }
+            }
         }
     }
 
