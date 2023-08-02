193cd7fa5c0d85745bb213553de174d9f9c9e845
cyjin0507 <cyjin0507@gmail.com>
2023-08-02 15:49:18 +0900

    Update Graph.js

--- 광주/B/js/Graph.js
+++ 광주/B/js/Graph.js
@@ -9,13 +9,15 @@ class Graph {
         this.canvas = document.querySelector("#canvas")
         this.ctx = this.canvas.getContext("2d")
 
+        this.colorList = ['aquamarine','blueviolet','yellowgreen','brown','steelblue','slategray','seagreen','tomato']
+
         this.setGraph()
     }
 
     setGraph() {
         this.barWidth = 40
         this.barSpacing = 20
-        this.startX = 30
+        this.startX = 80
         this.startY = this.canvas.height - 50
         this.maxValue = Math.max(...this.data)
 
@@ -26,12 +28,12 @@ class Graph {
         for (var i = 0; i < this.data.length; i++) {
             var barHeight = (this.data[i] / this.maxValue) * (this.canvas.height - 100)
 
-            this.ctx.fillStyle = "blue"
+            this.ctx.fillStyle = this.colorList[i]
             this.ctx.fillRect(this.startX, this.startY - barHeight, this.barWidth, barHeight)
 
-            this.ctx.fillStyle = 'red'
-            this.ctx.textAlign = "center";
-            this.ctx.fillText(this.name[i], this.startX + 20, 270)
+            this.ctx.fillText(this.name[i], 30, 18 + 20 * i)
+            this.ctx.rect(10,10 + 20 * i,10,10)
+            this.ctx.fill()
 
             this.startX += this.barWidth + this.barSpacing
         }
