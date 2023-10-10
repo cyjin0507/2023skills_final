bc936ba7df15057b1b1cd380cffa8208407d0eb7
choiyoungjin <cyjin0507@gmail.com>
2023-10-10 14:51:39 +0900

    c

--- 전남/B 복사본/js/map/Map.js
+++ 전남/B 복사본/js/map/Map.js
@@ -1,4 +1,4 @@
-const data = [
+const datas = [
     {name:"독립기념관", image:"/image/명소/image1.jpg", x:385, y:482},
     {name:"유관순열사 사적지", image:"/image/명소/image2.jpg", x:570, y:505},
     {name:"천안 삼거리 공원", image:"/image/명소/image3.jpg", x:280, y:430},
@@ -12,12 +12,11 @@ const data = [
 export default class Map {
     constructor() {
         this.WIDTH = 800
-        this.HEIGHT = 850
+        this.HEIGHT = 800
 
         this.tooltip = document.querySelector('.tooltipEl')
         this.canvas = document.querySelector('#map-canvas')
         this.ctx = this.canvas.getContext('2d')
-
         this.canvas.width = this.WIDTH
         this.canvas.height = this.HEIGHT
 
@@ -25,24 +24,24 @@ export default class Map {
     }
 
     init() {
-        this.pathList = data.map(({x,y})=> {
+        this.pointList = datas.map(({x,y})=> {
             const path = new Path2D()
             path.arc(x,y,10,0,Math.PI*2)
             return path
         })
 
         this.history = []
-        this.hoverIdx = -1
         this.selectIdx = -1
+        this.hoverIdx = -1
 
-        this.addEvent()
         this.drawing()
+        this.addEvent()
     }
 
     addEvent() {
-        this.canvas.addEventListener('mousemove', this.drawing.bind(this))
-        this.canvas.addEventListener('mousedown', this.onmousedown.bind(this))
-        this.canvas.addEventListener('mouseup', this.onmouseup.bind(this))
+        this.ctx.canvas.addEventListener('mousemove', this.drawing.bind(this))
+        this.ctx.canvas.addEventListener('mousedown', this.onmousedown.bind(this))
+        this.ctx.canvas.addEventListener('mouseup', this.onmouseup.bind(this))
 
         document.querySelector('#reset').addEventListener('click', this.reset.bind(this))
         document.querySelector('#undo').addEventListener('click', this.undo.bind(this))
@@ -51,34 +50,36 @@ export default class Map {
 
     drawing({offsetX, offsetY} = {}) {
         this.ctx.clearRect(0,0,this.WIDTH,this.HEIGHT)
-
         this.tooltip.style.display = 'none'
-        this.canvas.style.cursor = 'default'
+        this.ctx.canvas.style.cursor = 'default'
 
         this.hoverIdx = -1
 
         this.ctx.lineWidth = 3
-        this.ctx.fillStyle = '#ff8181'
+        this.ctx.fillStyle = 'red'
 
+        this.ctx.beginPath()
         for(let i=0; i<this.history.length-1; i++) {
-            const {x:prevX, y:prevY} = data[this.history[i]]
-            const {x:nextX, y:nextY} = data[this.history[i+1]]
+            const {x:prevX, y:prevY} = datas[this.history[i]]
+            const {x:nextX, y:nextY} = datas[this.history[i+1]]
 
-            this.ctx.beginPath()
-            this.ctx.moveTo(prevX, prevY)
-            this.ctx.lineTo(nextX, nextY)
-            this.ctx.stroke()
-            this.ctx.closePath()
+            if(i==0) {
+                this.ctx.moveTo(prevX, prevY)
+            } else {
+                this.ctx.lineTo(nextX, nextY)
+            }
         }
+        this.ctx.stroke()
+        this.ctx.closePath()
 
         if(this.selectIdx > -1) {
-            const {x:prevX, y:prevY} = data[this.selectIdx]
-            const findIdx = this.pathList.findIndex(path=>this.ctx.isPointInPath(path, offsetX, offsetY))
+            const {x:prevX, y:prevY} = datas[this.selectIdx]
+            const findIdx = this.pointList.findIndex(path=>this.ctx.isPointInPath(path, offsetX, offsetY))
 
             this.ctx.beginPath()
             this.ctx.moveTo(prevX, prevY)
             if(findIdx > -1) {
-                this.ctx.lineTo(data[findIdx].x, data[findIdx].y)
+                this.ctx.lineTo(datas[findIdx].x,datas[findIdx].y)
             } else {
                 this.ctx.lineTo(offsetX, offsetY)
             }
@@ -86,69 +87,68 @@ export default class Map {
             this.ctx.closePath()
         }
 
-        for(const index in this.pathList) {
-            const path = this.pathList[index]
+        for(const index in this.pointList) {
+            const path = this.pointList[index]
             if(this.ctx.isPointInPath(path, offsetX, offsetY)) {
                 this.hoverIdx = index
             }
-            this.ctx.fillStyle = this.hoverIdx == index ? 'yellow' : '#ff8181'
+            this.ctx.fillStyle = this.hoverIdx == index ? 'yellow' : 'red'
             this.ctx.fill(path)
         }
 
         for(const index in this.history) {
-            const {x,y} = data[this.history[index]]
-            this.ctx.textAlign = 'center'
+            const {x,y} = datas[this.history[index]]
             this.ctx.font = '14px Arial'
-            this.ctx.textBaseline = 'middle'
+            this.ctx.textAilgn = 'center'
+            this.ctx.textBaseLine = 'middle'
+            this.ctx.fillStyle = 'white'
 
             this.ctx.beginPath()
-            this.ctx.fillStyle = 'white'
             this.ctx.fillText(`${Number(index)+1}`, x, y)
             this.ctx.closePath()
         }
 
         if(this.hoverIdx > -1) {
-            const {x,y,name,image} = data[this.hoverIdx]
+            const {x,y,image,name} = datas[this.hoverIdx]
 
             this.tooltip.style.display = 'block'
-            this.tooltip.style.left = `${x + 10}px`
-            this.tooltip.style.top = `${y + 10}px`
+            this.tooltip.style.left = `${x+10}px`
+            this.tooltip.style.top = `${y+10}px`
             this.tooltip.querySelector('.name').innerText = name
             this.tooltip.querySelector('img').src = image
 
-            this.canvas.style.cursor = 'pointer'
+            this.canvas.style.cursor = 'cursor'
         }
 
     }
 
     onmousedown({offsetX, offsetY}) {
-        if(this.history.length == data.length) {
-            alert("경로설정을 마쳤습니다. 수정하고 싶으면 되돌리거나 초기화 하십시오.")
+        if(this.history.length >= 8) {
+            alert("이미 다 끝남")
             return
         }
 
-        const findIdx = this.pathList.findIndex(path=>this.ctx.isPointInPath(path, offsetX, offsetY))
+        const findIdx = this.pointList.findIndex(path=>this.ctx.isPointInPath(path, offsetX, offsetY))
         this.selectIdx = findIdx
 
         if(findIdx > -1) {
-            if(this.history.length != 0 &&
-                this.history[this.history.length-1] != this.selectIdx) {
-                alert("이미 설정된 경로입니다.")
-                this.selectIdx = -1
+            if(this.history.length != 0 && this.history[this.history.length-1] != this.selectIdx) {
+                alert("오류 1")
+                return
             }
         }
     }
 
     onmouseup({offsetX, offsetY}) {
-        const findIdx = this.pathList.findIndex(path=>this.ctx.isPointInPath(path, offsetX, offsetY))
-
-        if(findIdx == this.selectIdx) {
+        const findIdx = this.pointList.findIndex(path=>this.ctx.isPointInPath(path, offsetX, offsetY))
+        
+        if(this.selectIdx == findIdx) {
             this.selectIdx = -1
             return
         }
 
         if(this.history.includes(findIdx)) {
-            alert("이미 설정된 경로입니다.")
+            alert("오류 2")
             this.selectIdx = -1
             return
         }
@@ -164,55 +164,53 @@ export default class Map {
     }
 
     download() {
-        if(this.history < 1) {
-            alert("먼저 경로 설정을 해주세요.")
+        if(this.history.length < 1) {
+            alert("다운불가")
             return
         }
 
-        const newCanvas = document.createElement('canvas')
-        const newCtx = newCanvas.getContext('2d')
-        newCanvas.width = this.WIDTH
-        newCanvas.height = this.HEIGHT
+        const canvas = document.querySelector('canvas')
+        const ctx = canvas.getContext('2d')
+        canvas.width = this.WIDTH
+        canvas.height = this.HEIGHT
 
-        const image = document.querySelector('#map > img')
+        const pathImage = document.querySelector("#map > img")
+        
+        ctx.beginPath()
+        ctx.fillStyle = 'black'
+        ctx.fillRect(0,0,this.WIDTH,this.HEIGHT)
+        ctx.drawImage(pathImage,0,0)
+        ctx.closePath()
 
-        
-        newCtx.beginPath()
-        newCtx.fillStyle = 'black'
-        newCtx.fillRect(0,0,this.WIDTH,this.HEIGHT)
-        newCtx.closePath()
-        
-        newCtx.drawImage(image,0,0)
-        const pathImage = new Image()
-        pathImage.onload = () => {
-            newCtx.drawImage(pathImage,0,0)
+        const image = new Image()
+        image.onload = () => {
+            ctx.drawImage(image,0,0)
             const a = document.createElement('a')
-            a.href = newCanvas.toDataURL()
-            a.download = 'route.png'
+            a.href = canvas.toDataURL('image/jpg')
+            a.download = "map.png"
             a.click()
         }
+        image.src = this.canvas.toDataURL('image/jpg')
 
-        pathImage.src = this.canvas.toDataURL()
     }
 
     reset() {
         if(this.history.length < 1) {
-            alert("더 이상 진행 할 수 없습니다.")
+            alert("더이상 진행불가")
             return
         }
 
         this.history = []
         this.drawing()
     }
-    
+
     undo() {
         if(this.history.length < 1) {
-            alert("더 이상 진행 할 수 없습니다.")
+            alert("더이상 진행불가")
             return
         }
 
         this.history.splice(-1,1)
         this.drawing()
     }
-
 }
